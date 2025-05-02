cf53ff19526b71ca2ea2513b03f17d9758481133
Abubakar-Meigag <113248042+Abubakar-Meigag@users.noreply.github.com>
2025-05-02 18:40:07 +0100

    update install node express and all needs

---
+++ server/index.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+const cors = require("cors");
+const port = process.env.PORT || 6006;
+const pool = require("./database/db");
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+pool.connect();
+
+app.listen(port, () => {
+  console.log(`Server is running on Port: ${port}`);
+});
+
+app.get("/", (req, res) => {
+  res.send(`Welcome to Final Project server on port  ${port}`);
+});
